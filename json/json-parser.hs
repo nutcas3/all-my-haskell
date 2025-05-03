21b84c58aab177decbfe95b2cd6f960d281bf256
nutcas3 <mauricee423@gmail.com>
2025-05-03 12:19:40 +0300

    ``` feat: Implement JSON object parsing
    Adds a parser for JSON objects (`jsObject`).
    
    The parser handles:
    - Opening and closing curly braces `{}` with optional whitespace.
    - Key-value pairs separated by commas `,` with optional whitespace.
    - Each key-value pair consists of a JSON string as the key, followed by a colon `:`, and then any valid JSON value.
    
    This commit enables the parsing of complex JSON structures containing objects.
    ```

--- json/json-parser.hs
+++ json/json-parser.hs
@@ -24,3 +24,41 @@ data JSON = JSNull
           | JSArray [JSON]
           | JSObject [(String,JSON)]
           deriving (Show, Eq)
+
+
+json :: Parser JSON
+json = ws *> jsValue
+
+jsValue :: Parser JSON
+jsValue = choice [jsNull,jsBool,jsObject,jsArray,jsNumber,jsString
+
+-- |
+--
+-- >>> parseJSON "  null "
+-- Right JSNull
+jsNull :: Parser JSON
+jsNull = jsAtom "null" JSNull
+
+-- |
+--
+-- >>> parseJSON "  false "
+-- Right (JSBool False)
+-- >>> parseJSON "true"
+-- Right (JSBool True)
+jsBool :: Parser JSON
+jsBool = jsAtom "false" (JSBool False)
+     <|> jsAtom "true"  (JSBool True)
+
+-- |
+--
+-- >>> parseJSON "  { \"key1\": 2 ,  \"key2\" : false } "
+-- Right (JSObject [("key1",JSNumber 2),("key2",JSBool False)])
+jsObject :: Parser JSON
+jsObject = JSObject <$> betweenWs '{' kvs '}'
+  where
+    kvs = kv `sepBy` charWs ','
+    kv = do
+        JSString key <- jsString
+        charWs ':'
+        val <- jsValue
+        return (key, val)
